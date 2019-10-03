a7458bdd78069242292e70c4871616e7605b0ca0
John B <john@nexys.ch>
2019-10-03 10:58:25 +0200

    added deserialize

--- src/url.js
+++ src/url.js
@@ -15,3 +15,17 @@ export const getGoogleMapsAddressLink = ({ street, zip, city, country }) => {
  * @return p1=a1&p2=a2&...
  */
 export const paramsToString = params => Object.keys(params).map(key => key + '=' + encodeURIComponent(params[key])).join('&');
+
+/**
+ * @param k1=v1,k2=v2, ...
+ * @return { k1: v1, k2: v2 .. }
+ */
+export const deserialize = str => str.split(',').reduce((r, item) => {
+  const arr = item.split('=');
+
+  if (arr.length === 2) {
+    const [ key, value] = arr;
+
+    return  {...r, [key]: value};
+  } else return r;
+}, {});
--- src/url.test.js
+++ src/url.test.js
@@ -1,7 +1,13 @@
-import { paramsToString } from './url';
+import { paramsToString, deserialize } from './url';
 
 test('paramsToString', () => {
   const params = {a: 'fd', b: 23};
   const e = 'a=fd&b=23';
   expect(paramsToString(params)).toEqual(e); 
+});
+
+test('deserialize', () => {
+  const value = 'k1=v1,k2=v2';
+  const e = { 'k2': 'v2', 'k1': 'v1'};
+  expect(deserialize(value)).toEqual(e); 
 });
