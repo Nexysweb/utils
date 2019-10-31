6348693cf69e0a64ca884233c3b43f3193c856cd
John B <john@nexys.ch>
2019-10-31 16:21:18 +0100

    add tests for url

--- src/url.test.js
+++ src/url.test.js
@@ -1,4 +1,4 @@
-import { paramsToString, deserialize, replaceParams } from './url';
+import { paramsToString, deserialize, replaceParams, getGoogleMapsAddressLink } from './url';
 
 test('paramsToString', () => {
   const params = {a: 'fd', b: 23};
@@ -21,3 +21,10 @@ test('replaceParams', () => {
 
   expect(output).toEqual(outputExpected);
 });
+
+test('getGoogleMapsAddressLink', () => {
+  const address = { street: '1600 Amphitheatre Pkwy', city: 'Mountain View', zip: 94043, country: {name: 'United States'}};
+  const url = 'https://www.google.com/maps/?q=1600%20Amphitheatre%20Pkwy%2094043%20Mountain%20View%20United%20States'
+  expect(getGoogleMapsAddressLink(address)).toEqual(url);
+
+})
