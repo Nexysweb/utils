9a5307e8981bbcced003302754133a1e48e0c4a8
John B <john@nexys.ch>
2019-11-19 11:58:37 +0100

    added regex

---
+++ src/regex.js
@@ -0,0 +1,24 @@
+const isValid = (regex, str) => {
+  const re = new RegExp(regex, 'g');
+  const t = str.match(re);
+
+  return Array.isArray(t);
+}
+
+const getMatches = (regex, str, {flags='g', depth=0} = {}) => {
+  const re = new RegExp(regex, flags); 
+
+  // TODO: utils.match(str, regexp, options='g')
+  let match = null;
+  const matches = []
+  do {
+    match = re.exec(str);
+    if (match) {
+      matches.push(match[depth]);
+    }
+  } while (match != null);
+
+  return matches;
+}
+
+export { isValid, getMatches };
---
+++ src/regex.test.js
@@ -0,0 +1,13 @@
+import * as R from './regex';
+
+test('isValid', () => {
+  expect(R.isValid('^sal$', 'sal')).toEqual(true);
+});
+
+test('getMatches', () => {
+  expect(R.getMatches('^sal$', 'sal')).toEqual(['sal']);
+});
+
+test('getMatches 2', () => {
+  expect(R.getMatches('(\\d+)-(\\w+)-(\\d+)', '022-abc-789 043-fgh-465')).toEqual(['022-abc-789', '043-fgh-465']);
+});
