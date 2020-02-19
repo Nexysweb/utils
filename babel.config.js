54e64bd926818a4fd75bee8cf12f33f36d9a5108
John B <john@nexys.ch>
2020-02-19 14:47:35 +0100

    new babelasetup

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,3 @@
 const { BabelConfig } = require('@nexys/babelsetup');
 
-BabelConfig.plugins.push("@babel/plugin-transform-typescript");
-BabelConfig.presets.push('@babel/typescript');
-
 module.exports = BabelConfig;
