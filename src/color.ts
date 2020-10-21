49bb0ab92d59c3145106a2e4fff3b9abeb227313
John B <johan@nexys.ch>
2020-10-21 19:17:19 +0200

    Update color.ts

--- src/color.ts
+++ src/color.ts
@@ -11,3 +11,6 @@ export const toRGBA = (hex: string, alpha: number = 1): string => {
     16
   )},${parseInt(hex.substring(5, 7), 16)},${alpha})`;
 };
+
+// https://css-tricks.com/snippets/javascript/random-hex-color/
+export const getRandomColor = (): string =>  '#' + Math.floor(Math.random() * 16777215).toString(16);
